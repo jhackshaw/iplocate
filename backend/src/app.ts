9879b0ee8c5fbd950530cd714cfa3c69e6d90f85
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2020-10-15 11:45:46 -0400

    fix: add cors headers to backend response

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,14 @@
 import fastify from "fastify";
+import cors from "fastify-cors";
 import router from "./router";
 
 export const build = (opts = {}) => {
   const app = fastify(opts);
+  if (process.env.FRONTEND_DOMAIN) {
+    app.register(cors, {
+      origin: process.env.FRONTEND_DOMAIN,
+    });
+  }
   app.register(router);
   return app;
 };

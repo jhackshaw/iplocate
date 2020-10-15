3750a970a5491adfb2e009e2e9f99051ae41f21d
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2020-10-15 07:49:57 -0400

    feat: add basic use persistent state hook

---
+++ frontend/src/hooks/usePersistentState.ts
@@ -0,0 +1,20 @@
+import { SetStateAction, useCallback, useEffect, useState } from "react";
+
+export const usePersistentState = <T>(
+  key: string,
+  defaultValue: any = undefined
+): [T, React.Dispatch<SetStateAction<T>>] => {
+  const [value, setValue] = useState<T>(() => {
+    const existing = localStorage.getItem(key);
+    if (existing) {
+      return JSON.parse(existing);
+    }
+    return defaultValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+
+  return [value, setValue];
+};
